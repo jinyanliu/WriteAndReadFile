18e5d2b873be34cfbec70ff1a5da60e3745d19b5
Jinyan Liu <jinyan.liu@truecaller.com>
2019-09-10 17:07:12 +0200

    Write and read an object to/from file.

--- app/src/main/java/com/example/android/writeandreadfile/MainActivity.kt
+++ app/src/main/java/com/example/android/writeandreadfile/MainActivity.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.BufferedReader
-import java.io.File
 import java.io.InputStreamReader
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +15,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        readAndWriteString()
+
+        readAndWriteObjectPerson()
+    }
+
+    private fun readAndWriteString() {
         val fileName = "BusinessCard"
         val fileContent =  "Coffee Cake Carrot Espresso"
 
         this.openFileOutput(fileName, Context.MODE_PRIVATE).use {
             it.write(fileContent.toByteArray())
         }
-
-
 
         val fileInputStream = this.openFileInput(fileName)
         val inputStreamReader = InputStreamReader(fileInputStream)
@@ -33,7 +38,23 @@ class MainActivity : AppCompatActivity() {
             stringBuilder.append(text)
         }
 
-        textView.text = stringBuilder.toString()
+        textViewOne.text = stringBuilder.toString()
+    }
 
+    private fun readAndWriteObjectPerson() {
+
+        val fileName = "BusinessPerson"
+        val p = Person()
+
+        ObjectOutputStream(this.openFileOutput(fileName, Context.MODE_PRIVATE)).use {
+            it.writeObject(p)
+            it.close()
+        }
+
+        ObjectInputStream(this.openFileInput(fileName)).use {
+            val result = it.readObject() as? Person
+            result?.let { textViewTwo.text = result.name }
+            it.close()
+        }
     }
 }
---
+++ app/src/main/java/com/example/android/writeandreadfile/Person.kt
@@ -0,0 +1,8 @@
+package com.example.android.writeandreadfile
+
+import java.io.Serializable
+
+class Person: Serializable {
+    var name = "Anne"
+    var age = 32
+}
